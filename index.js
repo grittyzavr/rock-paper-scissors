0ed58824da61c0aafcb94ec2962964cff194b7d8
grittyzavr <gritty1337@protonmail.com>
2022-11-23 11:37:12 +0300

    random prop of obj

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const hand = {
+    rock: 'img/rock.png',
+    scissors: 'img/scissors.png',
+    paper: 'img/paper.png'
+}
 function rock() {
-    console.log('rock');
+    let currentHand = 'rock';
 }
 function scissors() {
-    console.log('scissors');
+    let currentHand = 'scissors';
 }
 function paper() {
-    console.log('paper');
+    let currentHand = 'paper';
+}
+function pickRandomProperty(obj) {
+    let result;
+    let count = 0;
+    for (let prop in obj)
+        if (Math.random() < 1/++count)
+           result = prop;
+    return result;
 }
